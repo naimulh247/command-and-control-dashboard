936c7c794331fc1bde1aa858ad593f071f92ac04
Jimmy Kong <jameskong098@gmail.com>
2023-03-27 00:30:13 +0000

    node that subscribes to web client and publishes to cmd_vel

--- ros-api/src/Robot.py
+++ ros-api/src/Robot.py
@@ -11,8 +11,13 @@ class Robot:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.twist = Twist()
-            
+        self.web_input = rospy.Subscriber('cmd_vel_mux/input/teleop', Twist, self.web_input_callback)
+        #self.twist = Twist()
+    
+    def web_input_callback(self, data):
+        self.cmd_vel_pub.publish(data)
+
 rospy.init_node('Robot')
 follower = Robot()
+print("Robot.py node is running!")
 rospy.spin()
