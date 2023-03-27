67a2264159fee19cd1034330b9957c1d8f54ee19
James Kong <78178372+jameskong098@users.noreply.github.com>
2023-03-26 20:38:35 -0400

    Delete web-client directory

--- web-client/src/components/VideoFeed.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const VideoFeed = (props) => (
-    <iframe className='video-feed' title='robot-video-feed' src={`http://${props.ip}:8080/`}></iframe>
-)
-export default VideoFeed
