829aabff35b8362314009eb4cc0e851f8a4d1253
Jimmy Kong <jameskong098@gmail.com>
2023-04-01 21:06:51 +0000

    video feed is partially working, needs resizing

--- web_client/src/components/VideoFeed.jsx
+++ web_client/src/components/VideoFeed.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const VideoFeed = (props) => (
-    <iframe className='video-feed' title='robot-video-feed' src={`http://${props.ip}:8080/`}></iframe>
+    <iframe className='video-feed' title='robot-video-feed' src={`http://${props.ip}:8090/stream_viewer?topic=/camera/rgb/image_raw`}></iframe>
 )
 export default VideoFeed
