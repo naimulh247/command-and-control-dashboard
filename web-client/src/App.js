80d6ba87e2cbfbab91a17d0ef535aaca3ef57a02
naimulh247 <naimulh247@gmail.com>
2023-04-02 15:18:55 -0700

    add header, body, and footer componet to the app

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/template/Footer";
+import Header from "./components/template/Header";
+import Body from "./components/template/Body";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
