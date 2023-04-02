eccf765c83cb019c84474c7cac70b358727be3fe
naimulh247 <naimulh247@gmail.com>
2023-04-02 16:44:14 -0700

    create global props for ros variable for other components

--- web-client/src/components/pages/Home.jsx
+++ web-client/src/components/pages/Home.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import ROSConnect from '../ros-bridge/ROSConnect';
+import Teleop from '../ros-bridge/Teleop';
+import { Col, Container, Row } from 'react-bootstrap';
 
 class Home extends Component {
-    state = {  } 
+    constructor(props) {
+        super(props);
+        this.state = {
+            ros: null
+        }
+        this.setRos = this.setRos.bind(this);
+    }
+
+    setRos(ros) {
+        this.setState({ ros });
+        console.log(ros);
+    }
+
     render() { 
         return (
             <main>
-                <ROSConnect/>
+                <Container>
+                    <Row>
+                        <Col>
+                            <ROSConnect setRos={this.setRos}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Teleop ros={this.state.ros}/>
+                        </Col>
+                    </Row>
+                </Container>
             </main>
         );
     }
