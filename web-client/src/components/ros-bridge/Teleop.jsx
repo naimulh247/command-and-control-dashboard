24de390613ee9b05371f12f497f3f3404e468a66
naimulh247 <naimulh247@gmail.com>
2023-04-02 15:26:46 -0700

    initialize the teleop component

---
+++ web-client/src/components/ros-bridge/Teleop.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Teleop extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h2>Telop</h2>
+        );
+    }
+}
+ 
+export default Teleop;
