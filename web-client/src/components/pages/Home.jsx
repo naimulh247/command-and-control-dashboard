33df07f86039841369538d80eccfcac3c26b3357
naimulh247 <naimulh247@gmail.com>
2023-04-06 00:11:00 -0700

    initialize a map component

--- web-client/src/components/pages/Home.jsx
+++ web-client/src/components/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ROSConnect from '../ros-bridge/ROSConnect';
 import Teleop from '../ros-bridge/Teleop';
 import { Col, Container, Row } from 'react-bootstrap';
+import RobotState from '../ros-bridge/RobotState';
+import Map from '../ros-bridge/Map';
 
 class Home extends Component {
     constructor(props) {
@@ -14,10 +16,12 @@ class Home extends Component {
 
     setRos(ros) {
         this.setState({ ros });
-        console.log(ros);
+        // console.log(ros);
     }
 
     render() { 
+        // console.log(this.state.ros)
+        const {ros} = this.state
         return (
             <main>
                 <Container>
@@ -28,7 +32,15 @@ class Home extends Component {
                     </Row>
                     <Row>
                         <Col>
-                            <Teleop ros={this.state.ros}/>
+                            <Teleop ros={ros}/>
+                        </Col>
+                        <Col>
+                            <Map ros={ros}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <RobotState ros={ros}/>
                         </Col>
                     </Row>
                 </Container>
---
+++ web-client/src/components/ros-bridge/Map.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>Map</h1>);
+    }
+}
+ 
+export default Map;
