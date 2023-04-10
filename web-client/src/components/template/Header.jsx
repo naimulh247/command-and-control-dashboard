aea08daad6dcba04b4983edb72d53c1c3a430966
Jimmy Kong <jameskong098@gmail.com>
2023-04-10 22:14:26 +0000

    added a help page for installation and running

--- web-client/src/components/template/Header.jsx
+++ web-client/src/components/template/Header.jsx
@@ -12,6 +12,7 @@ class Header extends Component {
                         <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/help">Help</Nav.Link>
                         <Nav.Link href="/settings">Settings</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
