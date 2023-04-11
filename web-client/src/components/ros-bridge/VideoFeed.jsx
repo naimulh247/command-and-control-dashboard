70255ddda9021228141cdef314869c11adcc8860
Jimmy Kong <jameskong098@gmail.com>
2023-04-11 17:01:25 +0000

    convert function to class component for video feed

--- web-client/src/components/ros-bridge/VideoFeed.jsx
+++ web-client/src/components/ros-bridge/VideoFeed.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ros_config from '../../configs/ros_config';
 
-const VideoFeed = (props) => (
-    <iframe className='video-feed' title='robot-video-feed' src={`http://${props.ip}:${ros_config.VIDEO_WEB_SERVER_PORT}`}></iframe>
-)
-export default VideoFeed
+class VideoFeed extends Component {
+  render() {
+    return (
+      <iframe className='video-feed' title='robot-video-feed' src={`http://${this.props.ip}:${ros_config.VIDEO_WEB_SERVER_PORT}`}></iframe>
+    );
+  }
+}
+
+export default VideoFeed;
