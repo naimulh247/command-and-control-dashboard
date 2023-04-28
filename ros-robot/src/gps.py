e661164d4882c46d3be66ca44c468afb39cc62e8
Brandon J. Lacy <Z3R0@duck.com>
2023-04-28 16:12:43 -0400

    feat: resilient to timeout from gps

--- ros-robot/src/gps.py
+++ ros-robot/src/gps.py
@@ -30,11 +30,11 @@ class GPS:
             while True:
                 try:
                     s.connect((self.HOST, self.PORT))
+                    # The data is received in the RMC data format
+                    gps_data = s.recv(1024)
                     break
                 except ConnectionRefusedError:
                     continue
-            # The data is received in the RMC data format
-            gps_data = s.recv(1024)
 
         # Transform data into dictionary
         gps_keys = ['message_id', 'utc_time', 'status', 'latitude', 'ns_indicator', 'longitude', 'ew_indicator', 'speed_over_ground', 'course_over_ground', 'date', 'magnetic_variation', 'mode', 'checksum']
