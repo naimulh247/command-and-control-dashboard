21a0e7f85b9bc62c1cae11f31f627e043de9a659
Jimmy Kong <jameskong098@gmail.com>
2023-04-28 19:29:32 +0000

    update About, README, Footer

--- web-client/src/components/main/Footer.jsx
+++ web-client/src/components/main/Footer.jsx
@@ -5,7 +5,7 @@ class Footer extends Component {
     render() { 
         return (
             <Container className='text-center footer-style'>
-                <p className='footer-p-style'>Designed by Naimul, James, Brandon &copy; 2023</p>
+                <p className='footer-p-style'>Designed by Naimul Hasan, James Kong, Brandon Lacy &copy; 2023</p>
             </Container>
         );
     }
