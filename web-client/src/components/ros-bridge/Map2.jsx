4be641e9fba424da4d4648feb8e0863cebbadc86
naimulh247 <naimulh247@gmail.com>
2023-05-02 17:30:59 -0400

    update Map component to update gps location accurately

--- web-client/src/components/ros-bridge/Map2.jsx
+++ web-client/src/components/ros-bridge/Map2.jsx
@@ -4,17 +4,17 @@ import Map, { Marker } from 'react-map-gl';
 
 function Map2({ros}) {
     const [viewport, setviewport] = useState({
-        latitude: 42.366251,
-        longitude: -71.258724,
-        zoom: 19
+        // initialize in the Robotics Lab
+        latitude: 42.3660,
+        longitude: -71.2587,
+        // set the initial map zoom level
+        zoom: 17
     });
-
-    const [latitude, setLatitude] = useState(0);
-    const [longitude, setLongitude] = useState(0);
 
     useEffect(() => {
         if (!ros) return;
-    
+        
+        // subscribe to the gps data being published from the app through ros
         const gpsSubscriber = new window.ROSLIB.Topic({
           ros: ros,
           name: '/gps',
@@ -22,50 +22,30 @@ function Map2({ros}) {
         });
     
         gpsSubscriber.subscribe(message => {
-          const data = JSON.parse(message.data);
-          setLatitude(data.latitude);
-          setLongitude(data.longitude);
+            // console.log(message)
+            const data = JSON.parse(message.data);
+            // console.log(data.latitude, data.longitude, viewport.latitude)
+            // update the gps data
+            setviewport({
+                longitude: data.longitude,
+                latitude: data.latitude
+            })
         });
     
-        return () => {
-          gpsSubscriber.unsubscribe();
-        };
       }, [ros]);
 
     return (
-        //   <Map
-        //     {...viewport}
-        //     style={{width: 600, height: 400}}
-        //     mapStyle="mapbox://styles/mapbox/streets-v9"
-        //     mapboxAccessToken='pk.eyJ1IjoibmFpbXVsaCIsImEiOiJjbGdnMTR4MnUwNzBoM2RydjFveHQ0emVpIn0.Jeb-L3dsxMkt6v6HTI8QyQ'
-        //   />;
-        <Map
-            // initialViewState={{
-            // longitude: -100,
-            // latitude: 40,
-            // zoom: 3.5
-            // }}
 
-            // initialViewState={viewport}
-            // style={{ width: 600, height: 400 }}
-            // mapStyle="mapbox://styles/mapbox/streets-v9"
-            // mapboxAccessToken='pk.eyJ1IjoibmFpbXVsaCIsImEiOiJjbGdnMTR4MnUwNzBoM2RydjFveHQ0emVpIn0.Jeb-L3dsxMkt6v6HTI8QyQ'
-        
+        <Map
             {...viewport}
-            // width="100%"
-            // height="100%"
             mapStyle="mapbox://styles/mapbox/streets-v11"
             mapboxAccessToken='pk.eyJ1IjoibmFpbXVsaCIsImEiOiJjbGdnMTR4MnUwNzBoM2RydjFveHQ0emVpIn0.Jeb-L3dsxMkt6v6HTI8QyQ'
-            onViewportChange={viewport => setviewport(viewport)}
-
         >
             {/* place a marker on the map */}
             <Marker longitude={viewport.longitude} latitude={viewport.latitude}>
                 {/* <div style={{color: 'red', fontSize: 24}}>📍</div> */}
                 <div style={{ color: 'red', fontSize: 24 }}>🤖</div>
-
             </Marker>
-
         </Map>
     )
 }
